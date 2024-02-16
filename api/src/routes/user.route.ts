a9985c817085b341fc5ff60c4521afc699a20a39
thujuli <thujuli037@gmail.com>
2024-02-16 13:25:13 +0800

    feat(api): Introduce endpoint to get all users

--- api/src/routes/user.route.ts
+++ api/src/routes/user.route.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import { userUpdate, userDestroy } from "../controllers/user.controller";
+import {
+  userUpdate,
+  userDestroy,
+  userIndex,
+} from "../controllers/user.controller";
 import { verifyToken } from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.put("/:userId", verifyToken, userUpdate);
 router.delete("/:userId", verifyToken, userDestroy);
+router.get("/", verifyToken, userIndex);
 
 export default router;
